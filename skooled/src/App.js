4a3f148d01e60758b258264e8da86e1370240a49
mchristian <mchristiansolon@gmail.com>
2020-09-05 08:26:58 +0800

    imported material ui, react router & set up basic components

--- skooled/src/App.js
+++ skooled/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Login from './components/login/Login';
+import Gallery from './components/gallery/Gallery';
+import Stage from './components/stage/Stage';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Login />
+      <Gallery />
+      <Stage />
+    </div>
+  );
 }
 
 export default App;
---
+++ skooled/src/components/stage/Stage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Stage() {
+  return (
+    <div>
+      <h1>Stage</h1>
+    </div>
+  );
+}
+
+export default Stage;
